9bf29fc7e348a3ea32d51e8564f051fb3cf53df3
sheethal-hj <sheethaljambagi2599@gmail.com>
2020-06-07 23:07:06 +0530

    first commit

---
+++ admin_main.php
@@ -0,0 +1,65 @@
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="canonical" href="https://getbootstrap.com/docs/3.4/examples/starter-template/">
+    <title>Bookalious</title>
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+    
+  </head>
+  <body>
+<!--/.navigation bar  -->
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand  em-text" href="#">Bookalious</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Book</a></li>
+            <li><a href="#">User</a></li>
+<!--/.drop down  -->
+            <li><a href="#">Transaction</a></li>
+            <li><a href="#">Category</a></li>
+            <li><a href="#">Author</a></li>
+            <li><a href="#">Publisher</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+            
+              <li><a class="glyphicon glyphicon-log-out" href="admin_logout.php">Logout</a></li>
+  
+            </ul>
+        
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+<!--/Jumbotron -->
+    <div class="jumbotron">
+      <div class="container">
+      <h1>Welcome Admin!!!</h1>
+      
+    </div>
+  </div>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="js/bootstrap.js"></script>
+  </body>
+</html>
---
+++ connect.php
@@ -0,0 +1,30 @@
+<?php
+
+ 
+
+define('DB_SERVER','localhost');
+
+define('DB_USERNAME','root');
+
+define("DB_PASSWORD", '');
+
+define("DB_DATABASE",'bookalious');
+
+ 
+
+$link=mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+ 
+
+//if($link==true){
+
+   // echo "connection successful";
+
+ 
+
+//}
+
+ 
+
+?>
+
